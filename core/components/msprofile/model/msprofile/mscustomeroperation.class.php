087c45d82b92411c8a7138346b0621a4bc9ce1ff
bezumkin <bezumkin@yandex.ru>
2015-06-15 01:56:40 +0700

    - Removed old files - Fixed installer Version 1.0.2-pl1

--- core/components/msprofile/model/msprofile/mscustomeroperation.class.php
+++
@@ -1,2 +0,0 @@
-<?php
-class msCustomerOperation extends xPDOSimpleObject {}
--- core/components/msprofile/model/msprofile/mysql/mscustomeroperation.class.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once (dirname(dirname(__FILE__)) . '/mscustomeroperation.class.php');
-class msCustomerOperation_mysql extends msCustomerOperation {}
