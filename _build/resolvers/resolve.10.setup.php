cf668fb55c98de415a48c97b3665f77fc49bb88a
bezumkin <bezumkin@yandex.ru>
2015-08-19 20:34:19 +0700

    - Improved installation script for MODX 2.4.

--- _build/resolvers/resolve.10.setup.php
+++ _build/resolvers/resolve.10.setup.php
@@ -10,7 +10,6 @@ if (!function_exists('installPackage')) {
 			$provider = $modx->getObject('transport.modTransportProvider', 1);
 		}
 
-		$provider->getClient();
 		$modx->getVersionData();
 		$productVersion = $modx->version['code_name'] . '-' . $modx->version['full_version'];
 
@@ -122,14 +121,14 @@ if (!function_exists('downloadPackage')) {
 
 
 $success = false;
-switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+switch (@$options[xPDOTransport::PACKAGE_ACTION]) {
 	case xPDOTransport::ACTION_INSTALL:
 	case xPDOTransport::ACTION_UPGRADE:
 		/* @var modX $modx */
 		$modx = &$object->xpdo;
 		/* Checking and installing required packages */
 		$packages = array(
-			'miniShop2' => '2.1.10-pl',
+			'miniShop2' => '2.1.12-pl',
 		);
 
 		foreach ($packages as $package_name => $version) {
